bcfba30b9ab6b8786f2466f9f2ccdc64c2ce00da
Helen Hou-Sandi <helen.y.hou@gmail.com>
2017-12-05 13:05:58 -0500

    Plugin start: /ads.txt will display something.

---
+++ ads-txt.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Ads.txt
+ * Description: Manage your ads.txt file from within WordPress.
+ * Version:     0.1
+ * Author:      Helen Hou-Sandi, 10up
+ * Author URI:  http://10up.com
+ * License:     GPLv2 or later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Display the contents of /ads.txt when requested.
+ * @return void
+ */
+function tenup_display_ads_txt() {
+	$request = $_SERVER['REQUEST_URI'];
+	if ( '/ads.txt' === $request ) {
+		header( 'Content-Type: text/plain' );
+		echo 'this is ads.txt!';
+		die();
+	}
+}
+add_action( 'init', 'tenup_display_ads_txt' );
