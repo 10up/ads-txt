058237336e651112d05fbaa486bc8735ebc18e6f
Helen Hou-Sandi <helen.y.hou@gmail.com>
2017-12-05 23:20:16 -0500

    Basic form output.

--- inc/admin.php
+++ inc/admin.php
@@ -11,11 +11,29 @@ function admin_menu() {
 }
 add_action( 'admin_menu', __NAMESPACE__ . '\admin_menu' );
 
+/**
+ * Output the settings screen
+ * @return void
+ */
 function settings_screen() {
+	$setting = get_option( 'adstxt' );
 ?>
 
 <div class="wrap">
 	<h2>Ads.txt</h2>
+
+	<form action="options.php" method="post">
+		<?php settings_fields( 'adstxt' ); ?>
+		<?php settings_errors(); ?>
+
+		<p class="description">COPY: Ads.txt is a root-level file, etc.</p>
+
+		<textarea class="widefat"><?php echo esc_textarea( $setting ); ?></textarea>
+
+		<p class="submit">
+			<input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+		</p>
+	</form>
 </div>
 
 <?php
