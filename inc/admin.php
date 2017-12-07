b379e729d10dc163b7bac4425c733004645573c4
Helen Hou-Sandi <helen.y.hou@gmail.com>
2017-12-07 13:20:39 -0500

    Display error messages on setting screen.

--- inc/admin.php
+++ inc/admin.php
@@ -24,19 +24,39 @@ function settings_screen() {
 
 	$content = isset( $post->post_content ) ? $post->post_content : '';
 
+	$errors = get_post_meta( $post->ID, 'adstxt_errors', true );
+
 // Also need to display errors based on meta key
 // It's okay if they display again if they leave and come back, I think
 ?>
 
 <div class="wrap">
+<?php if ( ! empty( $errors ) ) : ?>
+	<div class="notice notice-error adstxt-errors">
+		<p><strong>Your Ads.txt contains the following issues:</strong></p>
+		<ul>
+			<?php foreach( $errors as $error ) {
+				echo '<li class="' . $error['type'] . '">';
+
+				/* translators: Error message output. 1: Line number, 2: Error message */
+				printf(
+					__( 'Line %1$s: %2$s', 'adstxt' ),
+					$error['line'],
+					$error['message']
+				);
+
+				echo '</li>';
+			} ?>
+		</ul>
+	</div>
+<?php endif; ?>
+
 	<h2><?php _e( 'Ads.txt', 'adstxt' ); ?></h2>
 
 	<form method="post" action="<?php echo admin_url( 'admin-post.php' ); ?>" class="adstxt-settings-form">
 		<input type="hidden" name="post_id" value="<?php echo esc_attr( $post->ID ); ?>" />
 		<input type="hidden" name="action" value="adstxt-save" />
 		<?php wp_nonce_field( 'adstxt_save' ); ?>
-
-		<p class="description"><?php _e( 'COPY: Ads.txt is a root-level file, etc.', 'adstxt' ); ?></p>
 
 		<textarea class="widefat code" rows="25" name="adstxt"><?php echo esc_textarea( $content ); ?></textarea>
 
