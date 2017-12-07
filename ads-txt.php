1689b80e40906f1b26f120edccac7e6830b7a9bc
Helen Hou-Sandi <helen.y.hou@gmail.com>
2017-12-07 13:22:48 -0500

    Use the new CPT for the actual display of /ads.txt

--- ads-txt.php
+++ ads-txt.php
@@ -25,12 +25,13 @@ include_once( __DIR__ . '/inc/save.php' );
 function tenup_display_ads_txt() {
 	$request = $_SERVER['REQUEST_URI'];
 	if ( '/ads.txt' === $request ) {
-		$setting = get_option( 'adstxt' );
+		$post_id = get_option( 'adstxt_post' );
 
 		// Will fall through if no option found, likely to a 404
-		if ( ! empty( $setting ) ) {
+		if ( ! empty( $post_id ) ) {
+			$post = get_post( $post_id );
 			header( 'Content-Type: text/plain' );
-			echo $setting;
+			echo $post->post_content;
 			die();
 		}
 	}
