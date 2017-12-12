413561b7ea3533fa03ce0c372cbd79799f4997a0
Adam Silverstein <adam@10up.com>
2017-12-12 15:15:22 -0500

    include doesn't require parenthesis (PHPCS)

--- ads-txt.php
+++ ads-txt.php
@@ -14,9 +14,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-include_once( __DIR__ . '/inc/post-type.php' );
-include_once( __DIR__ . '/inc/admin.php' );
-include_once( __DIR__ . '/inc/save.php' );
+require_once __DIR__ . '/inc/post-type.php';
+require_once __DIR__ . '/inc/admin.php';
+require_once __DIR__ . '/inc/save.php';
 
 /**
  * Display the contents of /ads.txt when requested.
