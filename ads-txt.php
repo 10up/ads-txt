42aba259b54096b6f0302edbb1a99e7440d317e2
Helen Hou-Sandi <helen.y.hou@gmail.com>
2017-12-18 12:23:55 -0500

    Let core handle the fatals via sandboxing instead of a kludgy `wp_die()`-based method.
    This reverts commit db10f85c6437fe8b2261915e82e368a977ca63ee.

--- ads-txt.php
+++ ads-txt.php
@@ -13,21 +13,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * Require PHP version 5.3+.
- *
- * @return void
- */
-function tenup_adstxt_php_version() {
-	if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
-		wp_die(
-			__( 'Ads.txt Manager requires PHP 5.3+.', 'ads-txt' ),
-			__( 'Error Activating', 'ads-txt' )
-		);
-	}
-}
-register_activation_hook( __FILE__, 'tenup_adstxt_php_version' );
-
 require_once __DIR__ . '/inc/post-type.php';
 require_once __DIR__ . '/inc/admin.php';
 require_once __DIR__ . '/inc/save.php';
