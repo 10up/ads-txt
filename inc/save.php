5c24c5dcf564b2746afb9b7b3024c65e38f67087
Darin Kotter <darin.kotter@my.eitc.edu>
2021-11-18 15:06:34 -0700

    Update comment

--- inc/save.php
+++ inc/save.php
@@ -104,7 +104,7 @@ function validate_line( $line, $line_number ) {
 	} elseif ( 0 === strpos( $line, '#' ) ) { // This is a full-line comment.
 		$sanitized = wp_strip_all_tags( $line );
 	} elseif ( 1 < strpos( $line, '=' ) ) { // This is a variable declaration.
-		// The spec currently supports CONTACT and SUBDOMAIN.
+		// The spec currently supports CONTACT, INVENTORYPARTNERDOMAIN and SUBDOMAIN.
 		if ( ! preg_match( '/^(CONTACT|SUBDOMAIN|INVENTORYPARTNERDOMAIN)=/i', $line ) ) {
 			$errors[] = array(
 				'line' => $line_number,
