9ea1bd10ea477e6212d0ad68cf7ec354a9a34bde
Max Lyuchin <maksim.lyuchin@10up.com>
2022-02-11 19:49:26 +0300

    Fix admin test: old WP 4.9 selectors to activate/deactivate plugin

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,8 +1,8 @@
 describe("Admin can login and make sure plugin is activated", () => {
   it("Can deactivate plugin and activate it back", () => {
     cy.visitAdminPage("plugins.php");
-    cy.get("#deactivate-ads-txt").click();
-    cy.get("#activate-ads-txt").click();
-    cy.get("#deactivate-ads-txt").should("be.visible");
+    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").click();
+    cy.get("#activate-ads-txt, tr[data-slug=ads-txt] .activate a").click();
+    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").should("be.visible");
   });
 });
