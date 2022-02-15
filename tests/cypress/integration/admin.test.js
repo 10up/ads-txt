f842a71db8b1a3adf36349494108709e1231b22d
Max Lyuchin <maxim.lyuchin@gmail.com>
2022-02-15 17:35:00 +0300

    Test permalinks structure

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -1,8 +1,18 @@
 describe("Admin can login and make sure plugin is activated", () => {
+  before(() => {
+    cy.setPermalinkStructure("/%postname%/");
+  });
+
+  it("Permalink structure works", () => {
+    cy.visit("/hello-world/");
+  });
+
   it("Can deactivate plugin and activate it back", () => {
     cy.visitAdminPage("plugins.php");
     cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").click();
     cy.get("#activate-ads-txt, tr[data-slug=ads-txt] .activate a").click();
-    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").should("be.visible");
+    cy.get("#deactivate-ads-txt, tr[data-slug=ads-txt] .deactivate a").should(
+      "be.visible"
+    );
   });
 });
