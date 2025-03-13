212906f94d8988e523a34e6b3ddbe1b105b4d23c
Sören Wünsch <soeren.wrede@parshipgroup.com>
2025-03-13 18:38:51 +0100

    Use the __DIR__ constant instead of calling dirname(__FILE__)

--- inc/admin.php
+++ inc/admin.php
@@ -21,7 +21,7 @@ function admin_enqueue_scripts( $hook ) {
 
 	wp_enqueue_script(
 		'adstxt',
-		esc_url( plugins_url( '/js/admin.js', dirname( __FILE__ ) ) ),
+		esc_url( plugins_url( '/js/admin.js', __DIR__ ) ),
 		array( 'jquery', 'wp-backbone', 'wp-codemirror' ),
 		ADS_TXT_MANAGER_VERSION,
 		true
@@ -29,7 +29,7 @@ function admin_enqueue_scripts( $hook ) {
 	wp_enqueue_style( 'code-editor' );
 	wp_enqueue_style(
 		'adstxt',
-		esc_url( plugins_url( '/css/admin.css', dirname( __FILE__ ) ) ),
+		esc_url( plugins_url( '/css/admin.css', __DIR__ ) ),
 		array(),
 		ADS_TXT_MANAGER_VERSION
 	);
